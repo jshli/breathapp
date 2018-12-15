aff10cff61b3aabf8d86445bd26edcd20fc0ec09
jshli <joshua.li@mu-aa.com>
2018-12-15 17:48:26 +1100

    Created callback function within main animation. Better, but not good

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-let breaths = 3;
+let breaths = 1;
+let currentBreaths = 1;
 
 var circle = document.querySelector('.circle');
 var arrow = document.getElementsByClassName('btn-arrow');
@@ -25,9 +26,22 @@ document.getElementById('decrease').addEventListener('click', decreaseBreathes);
 document.getElementById('increase').addEventListener('click', increaseBreathes);
 
 
+
 //breath animation section 
 const breathAnimation = anime.timeline({
     autoplay: false,
+    begin: function(anim){
+        console.log(currentBreaths);
+    },
+    complete: function(anim) {
+        currentBreaths ++;
+        console.log(currentBreaths);
+        if (currentBreaths < breaths && currentBreaths !== breaths) {
+            return breathAnimation.restart()
+        } else {
+            currentBreaths = 1;
+        }
+    }
 });
 
 breathAnimation
@@ -35,21 +49,19 @@ breathAnimation
     targets: circle,
     opacity: 1,
     scale: 10,
-    easing: 'easeOutCubic',
-    duration: 500,
+    duration: 500
 })
 .add({
     targets: circle,
     opacity: 1,
     scale: 10,
-    duration: 1000
+    duration: 1000,
 })
 .add({
     targets: circle,
     opacity: 0.6,
     scale: 1,
     duration: 1000,
-    easing:'easeInCubic'
 })
 .add({
     targets: circle,
@@ -59,15 +71,20 @@ breathAnimation
 })
 
 
+
 const startButton = document.getElementById('start-btn')
 startButton.onclick = breathAnimation.play;
+
+startButton.addEventListener('click', function() { 
+    console.log("click")
+    breathAnimation.play(); 
+});
 
 var arrowMouseOver = anime({
     targets: '.btn-arrow',
     borderColor: '#384661',
     autoplay: false,
     easing: 'easeOutCubic',
-    loops: true
 });
 
 var arrowMouseOut = anime({
@@ -75,16 +92,10 @@ var arrowMouseOut = anime({
     borderColor: '#FFF',
     autoplay: false,
     easing: 'easeInCubic',
-    loop: true,
 });
 
 startButton.onmouseover = arrowMouseOver.play;
 startButton.onmouseout = arrowMouseOut.play;
 
-//document.getElementById('start').addEventListener('click', breath);
 
-//breathe initiate button animations
-
-
-console.log(breaths);
 
